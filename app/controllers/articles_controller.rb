a8b7d1f336beb2396ad7c896edf51b6875d04d36
CREMA <crema@FAC1s-MacBookAir.local>
2020-12-02 14:59:56 +0900

    add association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-	before_action :find_article, only: [:index, :show]
-	def index
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
 
+	def index
+		@articles = Article.all.order("created_at DESC")
 	end
 
 	def show
@@ -9,11 +10,11 @@ class ArticlesController < ApplicationController
 	end
 
 	def new
-		@article = Article.new
+		@article = current_user.articles.build
 	end
 
 	def create
-		@article = Article.new(article_params)
+		@article = current_user.articles.build(article_params)
 
 		if @article.save 
 			redirect_to @article
